09635f36c2618e0be39b730ad5d63445f2bb8936
Firmansyah Yanuar <fyfirman@gmail.com>
2023-01-19 23:18:18 +0700

    chore: add tailwind plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  extends: ["fyfirman", "plugin:@tanstack/eslint-plugin-query/recommended"],
-  plugins: ["@tanstack/query"],
+  extends: [
+    "fyfirman",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  plugins: ["@tanstack/query", "tailwindcss"],
   rules: {
     // your overrides
   },
---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require("eslint-config-fyfirman/prettier"),
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
